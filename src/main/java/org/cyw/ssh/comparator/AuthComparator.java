17f6c985f4d78a0cbf14fcffc41344ea1b854160
yuwei'chen <yuweiing@gmial.com>
2016-05-12 17:36:32 +0800

    无

---
+++ src/main/java/org/cyw/ssh/comparator/AuthComparator.java
@@ -0,0 +1,15 @@
+package org.cyw.ssh.comparator;
+
+import java.util.Comparator;
+
+import org.cyw.ssh.model.po.Tauth;
+
+public class AuthComparator implements Comparator<Tauth> {
+
+	public int compare(Tauth o1, Tauth o2) {
+		int i1 = o1.getCseq() != null ? o1.getCseq().intValue() : -1;
+		int i2 = o2.getCseq() != null ? o2.getCseq().intValue() : -1;
+		return i1 - i2;
+	}
+
+}
