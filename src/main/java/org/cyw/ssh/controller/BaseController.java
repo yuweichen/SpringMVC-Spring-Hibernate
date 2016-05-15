38c56d3a6689335cc3755fc5f55cb8109c7416cc
yuwei'chen <yuweiing@gmial.com>
2016-05-15 14:18:03 +0800

    微信入口

--- src/main/java/org/cyw/ssh/controller/BaseController.java
+++ src/main/java/org/cyw/ssh/controller/BaseController.java
@@ -1,11 +1,7 @@
 package org.cyw.ssh.controller;
 
-import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.cyw.ssh.util.StringEscapeEditor;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
@@ -14,19 +10,14 @@ import org.springframework.web.bind.ServletRequestDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
-import org.springframework.web.context.request.ServletWebRequest;
-
-import com.alibaba.fastjson.JSON;
 
 @Controller
 @RequestMapping("/baseController")
-public class BaseController{
+public class BaseController {
 
 	@InitBinder
 	public void initBinder(ServletRequestDataBinder binder) {
-		
+
 		binder.registerCustomEditor(Date.class, new CustomDateEditor(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"), true));
 		// prevent xss attack
 		binder.registerCustomEditor(String.class, new StringEscapeEditor(true, false));
@@ -36,6 +27,5 @@ public class BaseController{
 	public String redirectJsp(@PathVariable String folder, @PathVariable String jspName) {
 		return "/" + folder + "/" + jspName;
 	}
-
 
 }
--- src/main/java/org/cyw/ssh/controller/weixin/ApiController.java
+++ src/main/java/org/cyw/ssh/controller/weixin/ApiController.java
@@ -1,13 +1,13 @@
 package org.cyw.ssh.controller.weixin;
 
-import java.util.UUID;
-
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.log4j.Logger;
 import org.cyw.ssh.controller.BaseController;
+import org.cyw.ssh.wechat.Wechat;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -17,10 +17,14 @@ public class ApiController extends BaseController {
 	public static final Logger logger = Logger.getLogger(ApiController.class);
 
 	@ResponseBody
-	@RequestMapping("/valid")
+	@RequestMapping(value = "/valid", method = RequestMethod.GET)
 	public String valid(HttpServletRequest request) {
-		System.err.println("-------"+request.getAttribute("echostr"));
-		return UUID.randomUUID().toString();
+
+		Wechat wechat = new Wechat(request);
+		String s = wechat.execute();
+		System.err.println(s);
+		//return s;
+		return request.getParameter("echostr");
 	}
-	
+
 }
